ab7b27fa773c2214cff68613b05ac1406f1cb760
siporeySaba <siporeysaba@gmail.com>
2026-06-10 16:19:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-const API = "https://script.google.com/macros/s/AKfycby1lozKAoUrxHMNpXVJxlqwmfgb0Iqd08lUm4jLpWutpvwUpHW-ZoYBv8By0v88EwX1/exec";
+async function api(action, params = {}) {
+  const url = new URL("https://script.google.com/macros/s/AKfycby1lozKAoUrxHMNpXVJxlqwmfgb0Iqd08lUm4jLpWutpvwUpHW-ZoYBv8By0v88EwX1/exec");
 
-async function load() {
-  try {
-    const res = await fetch(`${API}?action=stats&username=test`);
-    const data = await res.json();
+  url.searchParams.append("action", action);
 
-    console.log("DATA:", data);
+  Object.keys(params).forEach(k => {
+    url.searchParams.append(k, params[k]);
+  });
 
-    document.getElementById("app").innerHTML = `
-      <h1>נלמדו: ${data.learned}</h1>
-      <h1>דולגו: ${data.skipped}</h1>
-      <h1>סה״כ: ${data.total}</h1>
-    `;
-  } catch (e) {
-    console.log("ERROR:", e);
-    document.getElementById("app").innerHTML = "שגיאה בטעינה";
-  }
+  const res = await fetch(url.toString(), {
+    method: "GET",
+    mode: "cors"
+  });
+
+  return await res.json();
 }
-
-load();
