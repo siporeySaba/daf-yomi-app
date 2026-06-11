f34ad0e437e4a0cbd983f1900e7d10848d911340
siporeySaba <siporeysaba@gmail.com>
2026-06-11 18:22:23 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD51xwSJ2Z-xQtOmqVpYHIwjiE5NwGjNy4",
@@ -10,3 +11,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
