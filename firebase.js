1d9b0a3bf1a1bb6c984461649b6500ecba9fbfbd
siporeySaba <siporeysaba@gmail.com>
2026-06-11 18:09:02 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD51xwSJ2Z-xQtOmqVpYHIwjiE5NwGjNy4",
+  authDomain: "daf-yomi-app-c8bf6.firebaseapp.com",
+  projectId: "daf-yomi-app-c8bf6",
+  storageBucket: "daf-yomi-app-c8bf6.firebasestorage.app",
+  messagingSenderId: "781781233558",
+  appId: "1:781781233558:web:5bc2c87e9f39e47bbd77b7"
+};
+
+export const app = initializeApp(firebaseConfig);
