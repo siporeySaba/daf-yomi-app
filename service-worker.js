b1055bfdab5d89e2dcb3f2328afcc0fda5cebb20
siporeySaba <siporeysaba@gmail.com>
2026-06-14 14:39:03 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,37 @@
 const CACHE_NAME = 'daf-yomi-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/main.js',
-  '/firebase.js',
-  '/gemaraRenderer.js'
+  './',
+  './index.html',
+  './style.css',
+  './main.js',
+  './firebase.js',
+  './gemaraRenderer.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache).catch(() => {
-        console.log('Some assets could not be cached');
+      console.log('Cache opened');
+      return cache.addAll(urlsToCache).catch(err => {
+        console.log('Cache error:', err);
       });
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
+  // רק cache GET requests
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request).catch(() => {
-        return caches.match('/index.html');
+        return caches.match('./index.html');
       });
     })
   );
